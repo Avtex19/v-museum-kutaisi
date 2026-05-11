b4258f03e9d4eca10e137fa4eafa63ca3860edaf
Avtex19 <atukaberadze19@gmail.com>
2026-05-11 13:14:33 +0400

    feat(api): configure django-cors-headers for frontend integration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'exhibits'
+    'corsheaders',
+    'exhibits',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +54,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+CORS_ALLOWED_ORIGINS = True
 
 TEMPLATES = [
     {
